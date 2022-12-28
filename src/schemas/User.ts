6b073efca5f5e9bc5dffa334ad1fc97c1abfe4de
Pedro-HenriqueDev <ph1305803@gmail.com>
2022-12-27 22:40:51 -0300

    first commit

---
+++ src/schemas/User.ts
@@ -0,0 +1,22 @@
+import {Schema, model, Document} from 'mongoose'
+
+interface UserInterface extends Document {
+    email?: string,
+    firstName?: string,
+    lastName?: string,
+    fullName(): string
+}
+
+const UserSchema = new Schema ({
+    email: String,
+    firsName: String,
+    lastName: String,
+}, {
+    timestamps: true
+})
+
+UserSchema.methods.fullName = function(): string {
+    return `${this.firsName} ${this.lastName}`
+}
+
+export default model<UserInterface>('User', UserSchema)
